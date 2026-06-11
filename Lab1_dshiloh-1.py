d4cf0ee48685c66a8e566d6f94901a0197758126
Daniel Shiloh <dshiloh@cscc.edu>
2026-06-10 23:57:55 -0400

    Added check digit logic

--- Lab1_dshiloh-1.py
+++ Lab1_dshiloh-1.py
@@ -5,11 +5,30 @@ Validate UPC-A code
 June 10, 2026
 """
 
+def find_check_digit(upc):
+    
+    sum = 0
+
+    #sum odd positions
+
+    #odd sum x 3
+
+    #add even positions
+
+    #result modulo 10 = M
+
+    #check digit is 0 or 10-M
+
+    #return check digit
+
 #ask user for upc
+input_UPC: str = input("Enter UPC: ")
 
 #validate that input is 12 char, only numbers
     #if invalid, print error and ask again
 
 #use fxn to calculate correct check digit
+find_check_digit(input_UPC[:-1])
+    #compare returned check digit to actual 12th digit
 
-#inform user if full upc is valid
+#inform user if full upc is valid or invalid
