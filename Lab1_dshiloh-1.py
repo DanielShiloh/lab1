b330ec6a8ed748aa6297d2e6cb2a92329e0f1a0a
Daniel Shiloh <dshiloh@cscc.edu>
2026-06-10 23:49:11 -0400

    Initial logic

---
+++ Lab1_dshiloh-1.py
@@ -0,0 +1,15 @@
+"""
+UPC Validator
+Daniel Shiloh
+Validate UPC-A code
+June 10, 2026
+"""
+
+#ask user for upc
+
+#validate that input is 12 char, only numbers
+    #if invalid, print error and ask again
+
+#use fxn to calculate correct check digit
+
+#inform user if full upc is valid
